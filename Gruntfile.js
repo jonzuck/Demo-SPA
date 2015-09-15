ba2be97282aaeced3332dd45af6b87872daad894
frimmin <jz@frimmin.com>
2015-09-15 09:45:53 -0400

    reactivated Grunt concat file, minor styling and text

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function (grunt) {
 		},
 		jshint: {
 			// define the files to lint
-			src: ['Gruntfile.js', 'src/assets/js/*.js'],
+			src: ['Gruntfile.js'],
 			// configure JSHint (documented at http://www.jshint.com/docs/)
 			options: {
 				// more options here if you want to override JSHint defaults
