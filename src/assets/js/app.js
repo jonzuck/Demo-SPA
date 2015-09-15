7c05557165c7fab1b67496002dddbd35d37a7ec8
frimmin <jz@frimmin.com>
2015-09-15 13:53:31 -0400

    using requirejs to load jquery and plugin

---
+++ src/assets/js/app.js
@@ -0,0 +1,36 @@
+/* requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: '../../Demo-SPA/src/components',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+		app: "../app",
+		jquery:'jquery/dist/jquery.min',
+		bxslider: 'jquery.bxslider/jquery.bxslider.min'
+    }
+});
+
+// Start the main app logic.
+requirejs(['jquery', 'bxslider'],
+function   ($, bxslider) {
+    //jQuery, and bxslider module are all
+    //loaded and can be used here now.
+});
+*/
+
+requirejs.config({
+    "baseUrl": "../../Demo-SPA/src/assets/js/lib",
+    "paths": {
+     "app": "../app",
+     jquery: "jquery.min" 
+    },
+    "shim": {
+        "jquery.bxslider": ["jquery"]
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["../app/main"]);
