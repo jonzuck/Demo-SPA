101890f4c4a458fea2405af141e17260b7c25bc9
frimmin <jz@frimmin.com>
2015-09-21 16:25:02 -0400

    more interim changes to lazy script.

--- src/assets/js/lib/jquery.lazy.js
+++ src/assets/js/lib/jquery.lazy.js
@@ -1,19 +1,18 @@
 ; (function () {
-
+  
 $.fn.lazy = function(){
-  document.getElementById('body').addEventListener('click', function () {
+  window.addEventListener('scroll', function () {
     var merc = $("#Mercury");
     var win = $(window);
     var scrollToElement = merc.offset().top;
     var amountPageScrolled = win.scrollTop() + win.height();
     var inRange = amountPageScrolled > scrollToElement - 200;
-  
-//    alert(scrollToElement);
-//    alert(inRange);
+   console.log(amountPageScrolled);
    
     if (inRange) {
-     merc.attr('src', 'assets/img/Mercury.jpg');
-     console.log(merc);
+     var imgSrc = merc.attr('data-src');
+     merc.attr('src', imgSrc);
+
     }
   });
 };
