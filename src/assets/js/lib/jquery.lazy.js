9b92dc08a3b0b878a47bc28e0f0ddc49da6e37eb
frimmin <jz@frimmin.com>
2015-09-22 14:17:34 -0400

    added lazyloading examples of iframe and JSON

--- src/assets/js/lib/jquery.lazy.js
+++ src/assets/js/lib/jquery.lazy.js
@@ -2,6 +2,7 @@
 
   $.fn.lazy = function () {
     window.addEventListener('scroll', function () {
+      //for images and iframes, things with "src"
       var lazyItem = $('.lazyload');
       var lazyArray = $.makeArray(lazyItem);
       var win = $(window);
@@ -9,11 +10,20 @@
 
       for (var i = 0; i < lazyArray.length; i++) {
         var scrollToElement = $(lazyArray[i]).offset().top;
-        var inRange = amountPageScrolled > scrollToElement - 200;
+        var inRange = amountPageScrolled > scrollToElement + 400;
         if (inRange) {
           var imgSrc = lazyArray[i].getAttribute('data-src');
           lazyArray[i].setAttribute('src', imgSrc);
         }
+      }
+    
+      //for lazy-loading JSON info into an HTML element
+      var lazyHTML = $('.lazyHTML');
+      var scrollToHTML = lazyHTML.offset().top; //jQuery enabled
+      var inRangeHTML = amountPageScrolled > scrollToHTML + 100;
+      if (inRangeHTML) {
+        var obj = JSON.parse(window.galileo);
+        document.getElementById('galileo').innerHTML = obj.name + obj.significance + obj.inPopularCulture;
       }
     });
   };
