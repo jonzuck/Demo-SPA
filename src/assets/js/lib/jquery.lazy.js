8d6c3edc6ac84535cbb53b7b176d059b6b9c3561
frimmin <jz@frimmin.com>
2015-09-21 23:34:30 -0400

    creating array of lazy-loaded items.

--- src/assets/js/lib/jquery.lazy.js
+++ src/assets/js/lib/jquery.lazy.js
@@ -2,21 +2,23 @@
   
 $.fn.lazy = function(){
   window.addEventListener('scroll', function () {
-    var merc = $("#Mercury");
+    var lazyItem = $('[data-src]');
+    var lazyArray = $.makeArray( lazyItem );
     var win = $(window);
-    var scrollToElement = merc.offset().top;
+    var scrollToElement = lazyItem.offset().top;
     var amountPageScrolled = win.scrollTop() + win.height();
-    var inRange = amountPageScrolled > scrollToElement - 200;
+    var inRange = amountPageScrolled > scrollToElement +100;
    console.log(amountPageScrolled);
-   
+    console.log(lazyArray[7].getAttribute('data-src'));
     if (inRange) {
-     var imgSrc = merc.attr('data-src');
-     merc.attr('src', imgSrc);
-
+    
+       
+     for (var i = 0; i< lazyArray.length; i++) {
+     var imgSrc = lazyArray[i].getAttribute('data-src');
+     lazyArray[i].setAttribute('src', imgSrc);
+      }
     }
   });
 };
 
 })();
-
-
