951fa6995772847c6ab4409d5a243b5d1c5f1a4c
frimmin <jz@frimmin.com>
2015-09-24 18:15:36 -0400

    trimmed a picture of the moon, creating a round image.

--- src/assets/js/canvas.js
+++ src/assets/js/canvas.js
@@ -7,38 +7,48 @@
         context.shadowOffsetY = 3;
         context.shadowBlur = 3;
         context.shadowColor = 'rgba(0, 0, 0, 0.5)';
-        
+
         //yellowgreen rect
-        context.fillStyle ='yellowgreen';
+        context.fillStyle = 'yellowgreen';
         context.fillRect(30, 20, 120, 120);
-        
-        //red rect
-        context.fillStyle = '#ff0000';
-        context.fillRect(200, 80, 160, 160);
-        
+
+        //tiling rect 
+    /*    
+      var imageObj = new Image();
+      imageObj.onload = function() {
+        var pattern = context.createPattern(imageObj, 'repeat');
+
+        context.rect(200, 80, 240, 240);
+        context.fillStyle = pattern;
+        context.fill();
+      };
+      imageObj.src = 'http://burtleburtle.net/bob/tile/fatigue.gif';
+   */      
+        context.fillStyle = 'red';
+        context.fillRect(180, 60, 160, 160);
+
         //circle
         context.arc(360, 240, 160, 0, Math.PI * 2, false);
-        
-      var grd = context.createRadialGradient(260, 110, 10, 238, 50, 300);
-      // light blue
-      grd.addColorStop(0, '#8ED6FF');
-      // dark blue
-      grd.addColorStop(1, '#004CB3');        
+
+        var grd = context.createRadialGradient(260, 110, 10, 238, 50, 300);
+        // light blue
+        grd.addColorStop(0, '#8ED6FF');
+        // dark blue
+        grd.addColorStop(1, '#004CB3');
         context.strokeStyle = '#00ffff';
         context.stroke();
       }
-        context.fillStyle = grd;
-        context.fill();
-      
+      context.fillStyle = grd;
+      context.fill();
+
       //text
       context.font = '40pt Calibri';
       context.fillStyle = 'blue';
       context.fillText('Hello World!', 250, 380);
-      
 
       context.beginPath();
       context.moveTo(280, 120);
-      
+
       // squiggle
       // line 1
       context.lineTo(400, 260);
@@ -52,7 +62,7 @@
       context.lineWidth = 5;
       context.strokeStyle = 'green';
       context.stroke();
-      
+
       //quadratic and bezier control points
       context.beginPath();
       context.moveTo(430, 300);
@@ -60,21 +70,21 @@
       context.lineWidth = 5;
       context.strokeStyle = 'purple';
       context.stroke();
-      
+
       context.beginPath();
       context.moveTo(490, 60);
       context.lineTo(495, 60);
       context.lineWidth = 5;
       context.strokeStyle = 'purple';
-      context.stroke(); 
-      
+      context.stroke();
+
       context.beginPath();
       context.moveTo(500, 300);
       context.lineTo(505, 300);
       context.lineWidth = 5;
       context.strokeStyle = 'purple';
-      context.stroke();      
-      
+      context.stroke();
+
       // set line width for all line joins
       context.lineWidth = 20;
       context.strokeStyle = 'rgba(255,0,255,0.8)';
@@ -102,33 +112,33 @@
       context.lineTo(779, 150);
       context.lineJoin = 'bevel';
       context.stroke();
-      
+
       // maroon pseudosquare
       context.beginPath();
-      context.moveTo(600,250);
-      context.lineTo(650,250);
+      context.moveTo(600, 250);
+      context.lineTo(650, 250);
       context.lineWidth = 50;
       context.strokeStyle = 'rgba(153,0,0,0.7)';
       context.stroke();
-      
+
       //blue mostly rounded rectangle
       var rectWidth = 168;
       var rectHeight = 100;
       var rectX = 680;
-      var rectY = 100;     
+      var rectY = 100;
       var radius = 30;
-      
+
       context.beginPath();
-      context.moveTo(rectX,rectY);
+      context.moveTo(rectX, rectY);
       context.lineTo(rectX + rectWidth - radius, rectY);
       context.arcTo(
-        rectX + rectWidth, rectY, 
+        rectX + rectWidth, rectY,
         rectX + rectWidth, rectY + rectHeight,
-        radius, rectY+radius, radius);
-      context.lineTo(rectX + rectWidth, rectY+ rectHeight);
-      context.lineTo(rectX + radius, rectY+ rectHeight);
+        radius, rectY + radius, radius);
+      context.lineTo(rectX + rectWidth, rectY + rectHeight);
+      context.lineTo(rectX + radius, rectY + rectHeight);
       context.arcTo(
-        rectX, rectY + rectHeight, 
+        rectX, rectY + rectHeight,
         rectX, rectY,
         radius);
       context.lineTo(rectX, rectY);
@@ -137,32 +147,38 @@
       context.fill();
       context.strokeStyle = '#6699aa';
       context.stroke();
-      
+
       //yellow rectangle
       context.beginPath();
-      context.rect(780, 350, 200, 50);
-      context.fillStyle = 'yellow';
+      context.rect(780, 250, 200, 150);
+      // add linear gradient
+      var gradient = context.createLinearGradient(780, 250, 980, 400);
+      // white
+      gradient.addColorStop(0, '#ffffff');
+      // yellowgreen
+      gradient.addColorStop(1, '#99aa00');
+      context.fillStyle = gradient;
       context.fill();
       context.lineWidth = 1;
       context.strokeStyle = 'black';
-      context.stroke(); 
+      context.stroke();
+      
+      //moon
+    var moonImg = document.createElement('img');  
+    moonImg.src ='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/FullMoon2010.jpg/292px-FullMoon2010.jpg';
+    moonImg.onload = function() {
+    context.save();
+    context.beginPath();
+    context.arc(166,161,121, 0, Math.PI * 2, true);
+    context.closePath();
+    context.clip();
+
+    context.drawImage(moonImg, 20, 20, 292, 278);
+
+    context.beginPath();
+    //context.arc(0, 0, 0, 0, Math.PI * 2, true);
+    context.clip();
+    context.closePath();
+    context.restore();
+};
     }
-
-
-	var merc = document.querySelector('img');
-	merc.addEventListener('load', function(){
-		context.drawImage(merc, 50,240);
-		
-	/*	
-		var i,
-			newImg = context.getImageData(0, 0, 500,500),
-			newLen = newImg.data.length;
-		for (i = 0; i < newLen; i+=4) {
-			newImg.data[i] = 255 - newImg.data[i];
-			newImg.data[i+1] = 255 - newImg.data[i+1];
-			newImg.data[i+2] = 255 - newImg.data[i+2];
-		}
-		context.putImageData(newImg, 0, 0);
-	}, false);
-*/	
-	});
