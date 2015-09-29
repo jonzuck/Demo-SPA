07cd326e934ac00af50e17f4154db14eee695dae
frimmin <jz@frimmin.com>
2015-09-29 10:43:57 -0400

    added buttons, reset code, randomization.

--- src/assets/js/canvas.js
+++ src/assets/js/canvas.js
@@ -3,7 +3,7 @@
 // Go ahead, read it really. I'll wait.
 // Or, if you prefer, watch some Numberphile videos.
 // Now that you're familiar with tau, do you not agree that Math.TAU 
-// make a heck of a lot more sense than Math.PI?
+// makes a heck of a lot more sense than Math.PI?
 // Does anyoe really find it easier to think in fractions of a semicircle
 // than fractions of a circle?
 // I, for one, do not. 
@@ -14,20 +14,18 @@ var cv = document.getElementById('canvas');
 var cx = cv.getContext('2d');
 var cvH = cv.height;
 var cvW = cv.width;
-Math.TAU = Math.PI * 2;
+Math.TAU = Math.PI * 2; // Throwing in the tau. Get it? Tau ~ towel? 
 var coords = document.getElementById('coords');
 var rightDown = false;
 var leftDown = false;
 var intervalId = 0;
 var animFrame; //request/clear animation frame
-var running = false; // is the ball moving?
 
   // keypress info necessary for the paddle
   //set rightDown or leftDown if the right or left keys are down
   document.addEventListener('keydown', function (evt) {
     if (evt.which == 39) rightDown = true;
     else if (evt.which == 37) leftDown = true;
- 
   });
 
   //and unset them when the right or left key is released
@@ -39,12 +37,12 @@ var running = false; // is the ball moving?
 //make a ball 
 var ball = {
   radius: 10,
-  x: 35,
-  y: 35,
+  x: Math.floor(Math.random() * 800 + 1),
+  y: Math.floor(Math.random() * 40 + 1),
   h: 20,
   w: 20,
-  dirX: 5,
-  dirY: 1,
+  dirX: Math.floor(Math.random() * 7 + 4),
+  dirY: Math.floor(Math.random() * 3 + 1),
   color: 'goldenrod',
   draw: function () {
     cx.beginPath();
@@ -68,12 +66,18 @@ var paddle = {
   }
 };
 
-//clear
+//clear (for trailing animation effect)
 function clear() {
   cx.fillStyle = 'rgba(0, 0, 0, 0.3)';
   cx.fillRect(0, 0, cvW, cvH);
 }
-  
+
+//reset (for new game)
+function reset() {
+  cx.fillStyle = 'rgb(0, 0, 0)';
+  cx.fillRect(0, 0, cvW, cvH);
+  console.log(cx);
+}  
 //drawing movement and animation
 function draw() {
   clear();
@@ -89,12 +93,13 @@ function draw() {
   else if (leftDown) paddle.x -= 5;
 
   coords.innerHTML = ball.x + ', ' + ball.y;
+  // ball boundary and paddle interaction
   var hit =
     (ball.y === paddle.y - ball.h) &&
     (ball.x > paddle.x && ball.x < paddle.x + paddle.w);
   animFrame = window.requestAnimationFrame(draw); 
-  // ball boundary and paddle interaction
-   console.log(ball.x + ', ' + ball.y + ', ' + hit + ', ' + paddle.x + ', ' + paddle.y + ', ' + rightDown);
+
+//   console.log(ball.x + ', ' + ball.y + ', ' + hit + ', ' + paddle.x + ', ' + paddle.y + ', ' + rightDown);
   if (ball.x + ball.w > cvW || ball.x - ball.w < 0) {
     ball.dirX = -ball.dirX;
   }
@@ -103,17 +108,21 @@ function draw() {
   } if (ball.y > cvH) {
     alert('You lose!');
     cancelAnimationFrame(animFrame);
+    reset();
   }
 }
 
 //start / stop motion 
-cv.addEventListener('mouseover', function () {
-  animFrame = window.requestAnimationFrame(draw);
-  running = true;
-});
-cv.addEventListener('mouseout', function () {
+document.getElementById('start').addEventListener('click', function () {
   cancelAnimationFrame(animFrame);
-  running = false;
+  reset(); 
+  ball.x = Math.floor(Math.random() * 800 + 1);
+  ball.y = Math.floor(Math.random() * 40 + 1);
+  ball.dirX = Math.floor(Math.random() * 7 + 4);
+  ball.dir = Math.floor(Math.random() * 3 + 1);
+  ball.draw();
+  animFrame = window.requestAnimationFrame(draw);
 });
-
-ball.draw();
+document.getElementById('stop').addEventListener('click', function () {
+  cancelAnimationFrame(animFrame);
+});
